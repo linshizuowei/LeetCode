9fb83ea2e3fa58b12bcbe82c9df9093db1dd5310
wangyuliang <wangyuliang@papayamobile.cn>
2017-12-04 17:14:10 +0800

    LC77

---
+++ Combinations.py
@@ -0,0 +1,43 @@
+# -*- encoding: utf-8 -*-
+"""
+77
+Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+
+For example,
+If n = 4 and k = 2, a solution is:
+
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]
+
+"""
+
+
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        ans = []
+        stack = []
+        x = 1
+        while True:
+            l = len(stack)
+            if l == k:
+                ans.append(stack[:])
+            if l == k or x > n - k + l + 1:
+                if not stack:
+                    return ans
+                x = stack.pop() + 1
+            else:
+                stack.append(x)
+                x += 1
+so = Solution()
+print so.combine(1, 1)
